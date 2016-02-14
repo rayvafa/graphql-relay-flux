38f2e0bf3944253ecc60f810929aca9f60efb61a
Ray Vafa <vafatabar@gmail.com>
2016-02-14 16:43:17 +1100

    creating an API endpoint for mongodb

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 import express from 'express';
 
+import {MongoClient} from 'mongodb';
+
 let app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000);
+let db;
+MongoClient.connect('mongodb://admin:admin@ds061385.mongolab.com:61385/rgrjs', (err, database) => {
+	if (err) {
+		console.log(err);
+		throw err;
+	}
+
+	db = database;
+	app.listen(3000, () => console.log('Listening on port 3000...'));
+});
+
+app.get("/data/links", (req, res) => {
+	db.collection("links").find({}).toArray((err, links) => {
+		if(err) {
+			console.log(err);
+			throw err;
+		}
+
+		console.log(links);
+		res.json(links);
+	});
+});

fa3d31fb57f8b37d8305c5d2b4a32c7ec36b088c
Ray Vafa <vafatabar@gmail.com>
2016-02-14 16:42:36 +1100

    breaking Hello component into its own file

---
+++ public/js/Hello.js
@@ -0,0 +1,9 @@
+var React = require("react");
+
+class Hello extends React.Component {
+	render() {
+		return (<h3>Hello JSX</h3>);
+	}
+}
+
+export default Hello;
--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,5 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
-
-class Hello extends React.Component {
-	render() {
-		return (<h3>Hello JSX</h3>);
-	}
-}
+import Hello from './Hello.js';
 
 ReactDOM.render(<Hello />, document.getElementById('react'));
